c220fe4dbd4f7ce2feec3eef3db0622426d58b5b
Zachary <zcleary1@ksu.edu>
2016-10-10 10:24:20 -0500

    Added Horizontal Split UserControl

--- VisiBoole/DisplayHorizontalSplit.cs
+++ VisiBoole/DisplayHorizontalSplit.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +10,11 @@ using System.Windows.Forms;
 
 namespace VisiBoole
 {
-    public partial class Form1 : Form
+    public partial class DisplayHorizontalSplit : UserControl
     {
-        public Form1()
+        public DisplayHorizontalSplit()
         {
             InitializeComponent();
-        }
-
-        private void verticalToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
