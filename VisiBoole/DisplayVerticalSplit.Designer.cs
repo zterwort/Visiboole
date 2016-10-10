30674672e2d7d18f6cae4442b3fca160fa39592a
Zachary <zcleary1@ksu.edu>
2016-10-10 10:00:35 -0500

    Added Vertical Split UserControl

--- VisiBoole/DisplayVerticalSplit.Designer.cs
+++ VisiBoole/DisplayVerticalSplit.Designer.cs
@@ -28,60 +28,233 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tlpMain = new System.Windows.Forms.TableLayoutPanel();
-            this.pnlDisplay = new System.Windows.Forms.Panel();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.tlpMain.SuspendLayout();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DisplayVerticalSplit));
+            this.tabEditor = new System.Windows.Forms.TabControl();
+            this.tabEditorPage1 = new System.Windows.Forms.TabPage();
+            this.tabEditorPage2 = new System.Windows.Forms.TabPage();
+            this.rtfOutput = new System.Windows.Forms.RichTextBox();
+            this.btnTick = new System.Windows.Forms.Button();
+            this.updTickCount = new System.Windows.Forms.NumericUpDown();
+            this.horizontalToolStrip = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton6 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton7 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton8 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton9 = new System.Windows.Forms.ToolStripButton();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.tabEditor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.updTickCount)).BeginInit();
+            this.horizontalToolStrip.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tlpMain
+            // tabEditor
             // 
-            this.tlpMain.BackColor = System.Drawing.Color.Transparent;
-            this.tlpMain.CausesValidation = false;
-            this.tlpMain.ColumnCount = 1;
-            this.tlpMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tlpMain.Controls.Add(this.pnlDisplay, 0, 0);
-            this.tlpMain.Controls.Add(this.panel1, 0, 1);
-            this.tlpMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tlpMain.Location = new System.Drawing.Point(0, 0);
-            this.tlpMain.Name = "tlpMain";
-            this.tlpMain.RowCount = 2;
-            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 89F));
-            this.tlpMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11F));
-            this.tlpMain.Size = new System.Drawing.Size(800, 600);
-            this.tlpMain.TabIndex = 0;
+            this.tabEditor.Controls.Add(this.tabEditorPage1);
+            this.tabEditor.Controls.Add(this.tabEditorPage2);
+            this.tabEditor.Location = new System.Drawing.Point(3, 14);
+            this.tabEditor.Name = "tabEditor";
+            this.tabEditor.SelectedIndex = 0;
+            this.tabEditor.Size = new System.Drawing.Size(393, 583);
+            this.tabEditor.TabIndex = 0;
             // 
-            // pnlDisplay
+            // tabEditorPage1
             // 
-            this.pnlDisplay.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pnlDisplay.Location = new System.Drawing.Point(3, 3);
-            this.pnlDisplay.Name = "pnlDisplay";
-            this.pnlDisplay.Size = new System.Drawing.Size(794, 528);
-            this.pnlDisplay.TabIndex = 0;
+            this.tabEditorPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabEditorPage1.Name = "tabEditorPage1";
+            this.tabEditorPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabEditorPage1.Size = new System.Drawing.Size(385, 557);
+            this.tabEditorPage1.TabIndex = 0;
+            this.tabEditorPage1.Text = "tabPage1";
+            this.tabEditorPage1.UseVisualStyleBackColor = true;
             // 
-            // panel1
+            // tabEditorPage2
             // 
-            this.panel1.Location = new System.Drawing.Point(3, 537);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 60);
-            this.panel1.TabIndex = 1;
+            this.tabEditorPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabEditorPage2.Name = "tabEditorPage2";
+            this.tabEditorPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabEditorPage2.Size = new System.Drawing.Size(385, 557);
+            this.tabEditorPage2.TabIndex = 1;
+            this.tabEditorPage2.Text = "tabPage2";
+            this.tabEditorPage2.UseVisualStyleBackColor = true;
+            // 
+            // rtfOutput
+            // 
+            this.rtfOutput.Location = new System.Drawing.Point(402, 36);
+            this.rtfOutput.Name = "rtfOutput";
+            this.rtfOutput.Size = new System.Drawing.Size(395, 561);
+            this.rtfOutput.TabIndex = 1;
+            this.rtfOutput.Text = "";
+            // 
+            // btnTick
+            // 
+            this.btnTick.Location = new System.Drawing.Point(722, 3);
+            this.btnTick.Name = "btnTick";
+            this.btnTick.Size = new System.Drawing.Size(75, 23);
+            this.btnTick.TabIndex = 2;
+            this.btnTick.Text = "Tick";
+            this.btnTick.UseVisualStyleBackColor = true;
+            // 
+            // updTickCount
+            // 
+            this.updTickCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updTickCount.Location = new System.Drawing.Point(672, 4);
+            this.updTickCount.Name = "updTickCount";
+            this.updTickCount.Size = new System.Drawing.Size(44, 22);
+            this.updTickCount.TabIndex = 3;
+            // 
+            // horizontalToolStrip
+            // 
+            this.horizontalToolStrip.Dock = System.Windows.Forms.DockStyle.None;
+            this.horizontalToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripButton4,
+            this.toolStripButton5,
+            this.toolStripButton6,
+            this.toolStripButton7,
+            this.toolStripButton8,
+            this.toolStripButton9});
+            this.horizontalToolStrip.Location = new System.Drawing.Point(402, 4);
+            this.horizontalToolStrip.Name = "horizontalToolStrip";
+            this.horizontalToolStrip.Size = new System.Drawing.Size(219, 25);
+            this.horizontalToolStrip.TabIndex = 7;
+            this.horizontalToolStrip.Text = "tlbIndependentVariables";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "toolStripButton1";
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "toolStripButton2";
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton3.Text = "toolStripButton3";
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton4.Text = "toolStripButton4";
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton5.Text = "toolStripButton5";
+            // 
+            // toolStripButton6
+            // 
+            this.toolStripButton6.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton6.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton6.Image")));
+            this.toolStripButton6.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton6.Name = "toolStripButton6";
+            this.toolStripButton6.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton6.Text = "toolStripButton6";
+            // 
+            // toolStripButton7
+            // 
+            this.toolStripButton7.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton7.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton7.Image")));
+            this.toolStripButton7.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton7.Name = "toolStripButton7";
+            this.toolStripButton7.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton7.Text = "toolStripButton7";
+            // 
+            // toolStripButton8
+            // 
+            this.toolStripButton8.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton8.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton8.Image")));
+            this.toolStripButton8.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton8.Name = "toolStripButton8";
+            this.toolStripButton8.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton8.Text = "toolStripButton8";
+            // 
+            // toolStripButton9
+            // 
+            this.toolStripButton9.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton9.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton9.Image")));
+            this.toolStripButton9.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton9.Name = "toolStripButton9";
+            this.toolStripButton9.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton9.Text = "toolStripButton9";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(314, 3);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 8;
+            this.btnRun.Text = "Run";
+            this.btnRun.UseVisualStyleBackColor = true;
             // 
             // DisplayVerticalSplit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.tlpMain);
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.horizontalToolStrip);
+            this.Controls.Add(this.updTickCount);
+            this.Controls.Add(this.btnTick);
+            this.Controls.Add(this.rtfOutput);
+            this.Controls.Add(this.tabEditor);
             this.Name = "DisplayVerticalSplit";
             this.Size = new System.Drawing.Size(800, 600);
-            this.tlpMain.ResumeLayout(false);
+            this.tabEditor.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.updTickCount)).EndInit();
+            this.horizontalToolStrip.ResumeLayout(false);
+            this.horizontalToolStrip.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TableLayoutPanel tlpMain;
-        private System.Windows.Forms.Panel pnlDisplay;
-        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabControl tabEditor;
+        private System.Windows.Forms.TabPage tabEditorPage1;
+        private System.Windows.Forms.TabPage tabEditorPage2;
+        private System.Windows.Forms.RichTextBox rtfOutput;
+        private System.Windows.Forms.Button btnTick;
+        private System.Windows.Forms.NumericUpDown updTickCount;
+        private System.Windows.Forms.ToolStrip horizontalToolStrip;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
+        private System.Windows.Forms.ToolStripButton toolStripButton6;
+        private System.Windows.Forms.ToolStripButton toolStripButton7;
+        private System.Windows.Forms.ToolStripButton toolStripButton8;
+        private System.Windows.Forms.ToolStripButton toolStripButton9;
+        private System.Windows.Forms.Button btnRun;
     }
 }
