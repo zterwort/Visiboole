92d5bc1aebe1be599a2644fb2fc14481a3d9c816
zterwort <zterwort@github.com>
2016-12-15 01:22:35 -0600

    Initial commit

---
+++ Visiboole/Views/DisplaySingleOutput.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VisiBoole.Controllers;
+
+namespace VisiBoole.Views
+{
+	public partial class DisplaySingleOutput : UserControl, IDisplay
+	{
+		private IDisplayController controller;
+
+		public DisplaySingleOutput()
+		{
+			InitializeComponent();
+		}
+
+		public Globals.DisplayType TypeOfDisplay
+		{
+			get
+			{
+				return Globals.DisplayType.OUTPUT;
+			}
+		}
+
+		public void AttachController(IDisplayController controller)
+		{
+			this.controller = controller;
+		}
+
+		public void LoadTabControl(TabControl tc)
+		{
+			return;
+		}
+
+		public void LoadWebBrowser(WebBrowser browser)
+		{
+			if (!(browser == null))
+			{
+				this.pnlMain.Controls.Add(browser, 0, 0);
+				browser.Dock = DockStyle.Fill;
+			}
+		}
+	}
+}
---
+++ VisibooleTests/Models/InputParserTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VisiBoole;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace VisiBoole.Tests
+{
+    [TestClass()]
+    public class InputParserTests
+    {
+        [TestMethod()]
+        public void InputParserTest()
+        {
+            InputParser inputParser = new InputParser();
+        }
+
+        [TestMethod()]
+        public void ParseInputTest()
+        {
+            string filename = "newFile1.vbi";
+            SubDesign subDesign = new SubDesign(filename);
+            FileInfo file = new FileInfo(filename);
+            InputParser inputParser = new InputParser();
+
+            inputParser.ParseInput(subDesign, null);
+            inputParser.ParseInput(subDesign, "A3");
+            inputParser.ParseInput(subDesign, "B0");
+
+        }
+
+        [TestMethod()]
+        public void ContainsVariableTest()
+        {
+            string filename = "newFile1.vbi";
+            SubDesign subDesign = new SubDesign(filename);
+            FileInfo file = new FileInfo(filename);
+            InputParser inputParser = new InputParser();
+            inputParser.ParseInput(subDesign, null);
+
+            inputParser.ContainsVariable("B2 = A3 A2 A1 A0", -1);
+            inputParser.ContainsVariable("", -1);
+            inputParser.ContainsVariable("A1", -1);
+            try
+            {
+                inputParser.ContainsVariable(null, -1);
+            }
+            catch
+            {
+
+            }
+
+        }
+
+        [TestMethod()]
+        public void SolveExpressionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void NegateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BinaryToDecimalTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
