b436131b4f4bfd295076ab206c432e1b8f6b72bc
Zachary <zcleary1@ksu.edu>
2017-02-21 10:47:20 -0600

    Initial Commit for the Parsing Engine
    Added a basic skeleton for the Parsing Engine

---
+++ ParsingEngine/SubExpression.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEngine
+{
+	/// <summary>
+	/// An Expression is comprised of a stack of SubExpressions, which are
+	/// code segments following the "=" sign in an Expression that have the
+	/// necessary components to be discretely computed.
+	/// </summary>
+	public class SubExpression
+	{
+	}
+}
